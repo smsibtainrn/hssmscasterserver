6bad0ac7b43ec896d3d09a0bcadd1cd803714932
sibtaingr8@gmail.com <sibtaingr8@gmail.com>
2017-05-01 13:47:12 +0500

    Change Made In index.js

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-var groupsApiResponses = '';
-app.set(groupsApiResponses, express.static(__dirname + '/api/groupsApiResponses'));
-app.use('/groups', groupsApiResponses);
+// var groupsApiResponses = '';
+// app.set(groupsApiResponses, express.static(__dirname + '/api/groupsApiResponses'));
+// app.use('/groups', groupsApiResponses);
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
