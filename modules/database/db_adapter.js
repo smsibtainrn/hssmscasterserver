f93cec7630c2b9032192a686bac6d1341501f7f5
sibtaingr8@gmail.com <sibtaingr8@gmail.com>
2017-05-01 22:28:02 +0500

    Removing Errors 5

--- modules/database/db_adapter.js
+++ modules/database/db_adapter.js
@@ -8,7 +8,7 @@ var config = {
     database: 'HSSMSCASTER',
     options: {encrypt: true, database: 'HSSMSCASTER'}
 //     ,port: 3000
-}
+};
 
 // var config = {
 //     user: 'sa',
@@ -36,50 +36,49 @@ var executeQuery = function (queryStr) {
 
 exports.executeQuery = executeQuery;
 //
-// exports.executeInBulk = function (queries) {
-//     return new Promise(function (resolve, reject) {
-//         // queries = []
-//         // queries = ["insert 1", "insert 2"]
-//         var qrIndex = 0;
-//         console.log("executeInBulk == start")
-//         var executeNext = function (query) {
-//             console.log(qrIndex + " --- " + query)
-//             executeQuery(query).then(function () {
-//                 if (qrIndex < queries.length - 1)
-//                     executeNext(queries[++qrIndex]);
-//                 else {
-//                     console.log("executeInBulk -- end")
-//                     //on complete
-//                     resolve(true);
-//                 }
-//             }).catch(function (err) {
-//                 console.log(!!err ? err : "mssql - unknown error");
-//
-//                 reject(err);
-//             });
-//         }
-//         executeNext(queries[qrIndex]);
-//     });
-// }
-//
-// exports.executeForSingleRecord = function (queryStr) {
-//     return new Promise(function (resolve, reject) {
-//         sql.connect(config).then(function () {
-//             new sql.Request().query(queryStr).then(function (response) {
-//                 resolve(response[0]);
-//             }).catch(function (err) {
-//                 console.log(!!err ? err : "mssql - unknown error");
-//
-//                 reject(err);
-//             });
-//         }).catch(function (err) {
-//             console.log(!!err ? err : "mssql - unknown error");
-//
-//             reject(err);
-//         });
-//     });
-// }
+exports.executeInBulk = function (queries) {
+    return new Promise(function (resolve, reject) {
+        // queries = []
+        // queries = ["insert 1", "insert 2"]
+        var qrIndex = 0;
+        console.log("executeInBulk == start")
+        var executeNext = function (query) {
+            console.log(qrIndex + " --- " + query)
+            executeQuery(query).then(function () {
+                if (qrIndex < queries.length - 1)
+                    executeNext(queries[++qrIndex]);
+                else {
+                    console.log("executeInBulk -- end")
+                    //on complete
+                    resolve(true);
+                }
+            }).catch(function (err) {
+                console.log(!!err ? err : "mssql - unknown error");
 
-// sql.on('error', function (err) {
-//     console.log(!!err ? err : "mssql - unknown error");
-// });
+                reject(err);
+            });
+        };
+        executeNext(queries[qrIndex]);
+    });
+};
+
+exports.executeForSingleRecord = function (queryStr) {
+    return new Promise(function (resolve, reject) {
+        sql.connect(config).then(function () {
+            new sql.Request().query(queryStr).then(function (response) {
+                resolve(response[0]);
+            }).catch(function (err) {
+                console.log(!!err ? err : "mssql - unknown error");
+                reject(err);
+            });
+        }).catch(function (err) {
+            console.log(!!err ? err : "mssql - unknown error");
+
+            reject(err);
+        });
+    });
+};
+
+sql.on('error', function (err) {
+    console.log(!!err ? err : "mssql - unknown error");
+});
