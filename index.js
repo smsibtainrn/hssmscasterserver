8cf9f30d394f0a8c0b9ea7a6f8fa74f016d36a97
sibtaingr8@gmail.com <sibtaingr8@gmail.com>
2017-05-01 17:01:39 +0500

    Change Made In index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+var groupsApiResponses = require('app/api/groupsApiResponses');
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
