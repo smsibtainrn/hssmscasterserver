36d2b6aa1af8ebac172ebf46a234f4952746f951
sibtaingr8@gmail.com <sibtaingr8@gmail.com>
2017-05-01 17:10:49 +0500

    Change Made In index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 // var groupsApiResponses = require(__dirname + '/api/groupsApiResponses');
 
-const groupsApiResponses = express.static(path.join(__dirname, '../api/groupsApiResponses'));
+const groupsApiResponses = express.static(path.join(__dirname, '/../api/groupsApiResponses'));
 app.use('/groups', groupsApiResponses);
 
 
