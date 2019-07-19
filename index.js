895367e6c2ab3fe95b5f5cd535a91c5ded03c164
smsrn <sibtain.raza@bykea.com>
2019-07-19 12:41:22 +0500

    Revert " Changes made for zendesk"
    This reverts commit 5d91f64ac5ccdd37d51eaab300ab2121db60c70c.

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ var bodyParser = require('body-parser');
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-// var check = require('./api/check');
-// app.use('/check', check);
+var check = require('./api/check');
+app.use('/check', check);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // API CLASSES - PATH DEFINE TO MAKE IS USABLE
 var usersApiResponses = require('./api/usersApiResponses');
-// var userContactsApiResponses = require('./api/userContactsApiResponses');
-// var groupsApiResponses = require('./api/groupsApiResponses');
-// var userSentMessagesApiResponses    =   require("./api/userSentMessagesApiResponses")
+var userContactsApiResponses = require('./api/userContactsApiResponses');
+var groupsApiResponses = require('./api/groupsApiResponses');
+var userSentMessagesApiResponses    =   require("./api/userSentMessagesApiResponses")
 
 // API CLASSES - INCLUDING IN APP
 app.use('/user', usersApiResponses);
-// app.use('/userContact', userContactsApiResponses);
-// app.use('/group', groupsApiResponses);
-// app.use("/userSentMessage",userSentMessagesApiResponses);
+app.use('/userContact', userContactsApiResponses);
+app.use('/group', groupsApiResponses);
+app.use("/userSentMessage",userSentMessagesApiResponses);
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -29,14 +29,14 @@ app.set('view engine', 'ejs');
 
 // app.use('/groups', require('app/api/groupsApiResponses'))
 
-// app.get('/', function(request, response) {
-//   response.render('pages/index');
-// });
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
 
-// app.get('/check', function(request, response) {
-//   response.send({is_executed:true,
-//     path:__dirname+'/api/groupsApiResponses/check1'});
-// });
+app.get('/check', function(request, response) {
+  response.send({is_executed:true,
+    path:__dirname+'/api/groupsApiResponses/check1'});
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
